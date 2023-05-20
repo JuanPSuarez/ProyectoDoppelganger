a8fe75f38d00343f7c97bb5d5ad8545701fd08e4
Juan Suarez <jp.juansuarez@gmail.com>
2023-05-20 15:55:45 -0300

    Added a version using the open ai API

--- withOpenIaAPi.py
+++ withOpenIaAPi.py
@@ -1,6 +1,4 @@
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment import SentimentIntensityAnalyzer
+import openai
 
 # Define greetings and regards
 greetings = ["Hola", "Hola que tal"]
@@ -8,13 +6,8 @@ regards = ["Chau", "Nos Vemos", "Abrazo"]
 formal_greetings = ["Estimados","Buenas","Buenas tardes"]
 formal_regards = ["Saludos cordiales", "Saludos"]
 
-# Initialize the sentiment analyzer
-sia = SentimentIntensityAnalyzer()
-
-# Function to calculate sentiment score
-def calculate_sentiment(text):
-    sentiment = sia.polarity_scores(text)
-    return sentiment["compound"]
+# Initialize the OpenAI API
+openai.api_key = "YOUR_API_KEY"
 
 # Function to check if a greeting is present in the text
 def has_greeting(text):
@@ -56,14 +49,50 @@ def calculate_points(email):
     if not has_formal_greeting(email) or not has_formal_regard(email):
         points += 2
 
-    # Calculate sentiment score
-    sentiment_score = calculate_sentiment(email)
-    if sentiment_score < 0.5:
+    # Get the intention of the message from the OpenAI API
+    response = openai.Completion.create(
+        engine="text-davinci-001",
+        prompt="What is the intention of the message?",
+        temperature=0.7,
+        top_p=0.9,
+        fine_tune="text-davinci-001",
+        input=email,
+    )
+
+    # Check if the intention is formal
+    if response["choices"][0]["text"].lower().startswith("formal"):
         points += 1
 
     return points
 
+# Function to compare two emails
+def compare_emails(email1, email2):
+    # Get the points for each email
+    points1 = calculate_points(email1)
+    points2 = calculate_points(email2)
+
+    # Compare the points
+    if points1 > points2:
+        return 1
+    elif points1 < points2:
+        return -1
+    else:
+        return 0
+
 # Example usage
-email_text = "Estimados, buenas tardes  Espero que todo vaya bien. Saludos cordiales."
-points = calculate_points(email_text)
-print("Points:", points)
+# Input the email of a persona
+persona_email = "Estimados, buenas tardes  Espero que todo vaya bien. Saludos cordiales."
+
+# Input the email of a user
+user_email = "Hola, como estas? Espero que todo bien. Abrazo"
+
+# Compare the emails
+result = compare_emails(persona_email, user_email)
+
+# Print the result
+if result == 1:
+    print("The emails are from different people.")
+elif result == -1:
+    print("The emails are from the same person.")
+else:
+    print("The emails cannot be compared.")
